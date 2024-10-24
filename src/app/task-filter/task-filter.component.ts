0653959727785b4d1142d8f41f3be42d127bfe5c
Matheus Castro <castrodepaulamatheus2@gmail.com>
2024-10-24 17:13:03 -0300

    criando html dos componentes

--- src/app/task-filter/task-filter.component.ts
+++ src/app/task-filter/task-filter.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-task-filter',
+  selector: 'app-task-filter', //O seletor é usado na importação de um componente para o outro. Esse código vai para os "imports[aqui]" do componente pai que irá puxa-lo.
   standalone: true,
   imports: [],
   templateUrl: './task-filter.component.html',
--- src/app/task-list/task-list.component.ts
+++ src/app/task-list/task-list.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { TaskFilterComponent } from "../task-filter/task-filter.component";
 
 @Component({
-  selector: 'app-task-list',
+  selector: 'app-task-list', 
   standalone: true,
-  imports: [],
+  imports: [TaskFilterComponent], //O seletor do componente do "task-filter" deve ser colocado aqui para possibilitar a utilização do componente no html.
   templateUrl: './task-list.component.html',
   styleUrl: './task-list.component.css'
 })
